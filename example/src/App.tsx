54387592c39862884cbee558dcadaf17b8a02561
Aaron Russell <alrussell90@icloud.com>
2024-12-15 00:58:33 -0800

    feat: switched to vite for development application instead of create react

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,19 +1,40 @@
-import { WysiwygEditor } from 'nodegeeks-react-wysiwyg-editor';
-import React from 'react';
-import './App.css';
+import { useState } from 'react'
+import { WysiwygEditor } from '../../src/WysiwygEditor'
+import './App.css'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
 
 function App() {
-  const [content, setContent] = React.useState("<p>Hello World!</p>");
-
+  const [count, setCount] = useState(0)
+  const [content, setContent] = useState('')
   return (
     <>
-    React App
+      <div>
+        <a href="https://vite.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Vite + React</h1>
       <WysiwygEditor
         content={content}
         onChange={setContent}
       />
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.tsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
     </>
-  );
+  )
 }
 
-export default App;
+export default App
