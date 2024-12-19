bb4aac04ea659f976b693f40db61546be3023033
Aaron Russell <alrussell90@icloud.com>
2024-12-19 00:54:38 -0800

    chore: updated build to use ES Module instead of Common JS

--- scripts/build.js
+++ scripts/build.js
@@ -1,38 +1,39 @@
-const fs = require('fs');
-const path = require('path');
+import { execSync } from 'child_process';
+import { copyFileSync, existsSync, mkdirSync, readdirSync, statSync } from 'fs';
+import { extname, join, relative } from 'path';
 
 function copyCssFiles() {
-    const srcDir = path.join(__dirname, '../src');
-    const distDir = path.join(__dirname, '../dist');
+    const srcDir = join(__dirname, '../src');
+    const distDir = join(__dirname, '../dist');
 
     // Directories to exclude
     const excludeDirs = ['__tests__', '__mocks__', 'tests', 'test'];
 
     // Create dist directory if it doesn't exist
-    if (!fs.existsSync(distDir)) {
-        fs.mkdirSync(distDir, { recursive: true });
+    if (!existsSync(distDir)) {
+        mkdirSync(distDir, { recursive: true });
     }
 
     function copyFiles(dir) {
-        const files = fs.readdirSync(dir);
+        const files = readdirSync(dir);
 
         files.forEach(file => {
-            const srcPath = path.join(dir, file);
-            const relativePath = path.relative(srcDir, srcPath);
-            const distPath = path.join(distDir, relativePath);
+            const srcPath = join(dir, file);
+            const relativePath = relative(srcDir, srcPath);
+            const distPath = join(distDir, relativePath);
 
             // Skip if the current path includes any of the excluded directories
             if (excludeDirs.some(excludeDir => srcPath.includes(excludeDir))) {
                 return;
             }
 
-            if (fs.statSync(srcPath).isDirectory()) {
-                if (!fs.existsSync(distPath)) {
-                    fs.mkdirSync(distPath, { recursive: true });
+            if (statSync(srcPath).isDirectory()) {
+                if (!existsSync(distPath)) {
+                    mkdirSync(distPath, { recursive: true });
                 }
                 copyFiles(srcPath);
-            } else if (path.extname(file) === '.css') {
-                fs.copyFileSync(srcPath, distPath);
+            } else if (extname(file) === '.css') {
+                copyFileSync(srcPath, distPath);
                 console.log(`Copied: ${relativePath}`);
             }
         });
@@ -42,7 +43,7 @@ function copyCssFiles() {
 }
 
 // Run TypeScript compilation first
-require('child_process').execSync('tsc', { stdio: 'inherit' });
+execSync('tsc', { stdio: 'inherit' });
 
 // Then copy CSS files
 copyCssFiles();
