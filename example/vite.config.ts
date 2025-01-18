93d09f8ae969991410b58f659ca029aed81e4d7b
Aaron Russell <alrussell90@icloud.com>
2025-01-18 15:26:00 +0800

    chore: reformatting eslint

--- example/vite.config.ts
+++ example/vite.config.ts
@@ -1,12 +1,12 @@
-import react from "@vitejs/plugin-react";
-import path from "path";
-import { defineConfig } from "vite";
+import react from '@vitejs/plugin-react'
+import path from 'path'
+import { defineConfig } from 'vite'
 
 export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
-      "nodegeeks-react-wysiwyg-editor": path.resolve(__dirname, "../src")
+      'nodegeeks-react-wysiwyg-editor': path.resolve(__dirname, '../src')
     }
   }
 })
--- src/components/Toolbar.tsx
+++ src/components/Toolbar.tsx
@@ -1,5 +1,5 @@
-import React, { useState } from 'react';
-import { TableStyles, ToolbarProps } from '../types/ToolbarTypes';
+import React, { useState } from "react";
+import { TableStyles, ToolbarProps } from "../types/ToolbarTypes";
 
 const icons = {
   bold: <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"><path d="M6 4h8a4 4 0 0 1 4 4 4 4 0 0 1-4 4H6z"></path><path d="M6 12h9a4 4 0 0 1 4 4 4 4 0 0 1-4 4H6z"></path></svg>,
